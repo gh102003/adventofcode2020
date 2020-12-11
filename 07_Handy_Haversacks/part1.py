94ab8f3b26cef493e72338f13442b011828fa355
George Howarth <george@pyesmeadow.com>
2020-12-11 19:03:10 +0000

    Day 7 more

--- 07_Handy_Haversacks/part1.py
+++ 07_Handy_Haversacks/part1.py
@@ -2,26 +2,40 @@ import os
 import re
 input_path = os.path.join(os.path.dirname(__file__), "input.txt")
 
-rules = []
+rules = {}
 with open(input_path, "r") as file:
-  rules = {line.split("contain")[0].strip(" .\n"): line.split("contain")[1].strip(" .\n") for line in file}
+    rules = {line.split("bags contain")[0].strip(" .\n"): line.split(
+        "contain")[1].strip(" .\n") for line in file}
 
-def can_contain_shiny_gold(bag):
-  if bag == "shiny_gold":
-    return True
-    
-  contents = rules[bag]
 
-  if contents == "no other bags":
+def can_contain_shiny_gold(bag, depth=0):
+    # print(" " * depth, bag)
+    if bag == "shiny gold":
+        return True
+
+    contents = rules[bag]
+
+    if contents == "no other bags":
+        return False
+    if "shiny gold" in contents:
+        return True
+    else:
+        for match in re.finditer(r"[1-9]+ ([a-z]+ [a-z]+) bags", contents):
+            if can_contain_shiny_gold(match.group(1), depth+1):
+                return True
+
     return False
 
-print(rules)
 
-# with open(input_path, "r") as file:
-  # for rule in file:
-    #print(rule.split("contain")[1].rstrip("\n"))
-    # for bag in can_contain_shiny_gold:
-    #   if re.search(bag, rule.split("contain")[1].rstrip("\n")):
-    #     can_contain_shiny_gold.append(rule.split(" bags contain")[0])
+# print(rules)
+# print()
 
-print(set(can_contain_shiny_gold))
+total = 0
+for bag in rules.keys():
+    if can_contain_shiny_gold(bag):
+        total += 1
+        print(f"{bag} can contain shiny gold\n")
+    else:
+        print(f"{bag} cannot contain shiny gold\n")
+
+print(f"{total=}")
